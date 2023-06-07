19f8f12ee1c625a55ee56d56fb70841da5e209a4
TAMADA01 <114475437+TAMADA01@users.noreply.github.com>
2023-06-08 01:19:16 +0300

    Вопрос 2

---
+++ Вопрос 2/AreaOfFigures/AreaOfFiguresLibrary/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AreaOfFiguresLibrary
+{
+    public abstract class Figure
+    {
+        public double Area { get; protected set; }
+
+        public Figure()
+        {
+        }
+
+        protected abstract double CalculateArea();
+    }
+
+    public class Curcle : Figure
+    {
+        public readonly double Radius;
+
+        public Curcle(double radius) : base() 
+        {
+            Radius = radius;
+            Area = CalculateArea();
+        }
+
+        protected override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Triangle : Figure
+    {
+        public readonly double SideA, SideB, SideC;
+
+        public Triangle(double a, double b, double c) : base()
+        {
+            SideA = a;
+            SideB = b;
+            SideC = c;
+            Area = CalculateArea();
+        }
+
+        protected override double CalculateArea()
+        {
+            double p = (SideA + SideB + SideC) / 2;
+
+            return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+        }
+    }
+}
---
+++ Вопрос 2/AreaOfFigures/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AreaOfFiguresLibrary;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure figure = new Curcle(5);
+
+            Console.WriteLine(figure.Area);
+        }
+    }
+}
